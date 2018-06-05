896e0d6e7e66bd41f9b01bcf35490ea3eeed52e4
Danilo Santos <dandesenvolvimento@gmail.com>
2018-06-05 16:03:41 -0300

    Initial implementation of Databases Page

--- src/pages/Databases.vue
+++ src/pages/Databases.vue
@@ -1,7 +1,30 @@
 <template>
-    <div>
-        Hello
-    </div>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">Databases</h1>
+            <button class="button is-success">Add New Database</button>
+            <table class="table is-striped is-hoverable is-fullwidth">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Size</th>
+                        <th># of Docs</th>
+                        <th>Update Seq</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
 </template>
 
 <script>
