cce47bd56e634121265f37abbcb15a5bb3a8a6ed
Danilo Santos <dandesenvolvimento@gmail.com>
2018-06-05 15:45:22 -0300

    Changing router mode to history

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 import HomePage from './pages/homepage/HomePage'
+import NotFoundComponent from './pages/NotFoundComponent'
 
 export default [
     { 
       path: '/', 
       component: HomePage 
-    }
+    },
+    { 
+      path: '*', 
+      component: NotFoundComponent }
   ];
