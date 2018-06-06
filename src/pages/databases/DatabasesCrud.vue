26f75035d19552989d075544ade605ef733406fc
Danilo Santos <dandesenvolvimento@gmail.com>
2018-06-06 18:28:44 -0300

    Refactored using modal component

--- src/pages/databases/DatabasesCrud.vue
+++ src/pages/databases/DatabasesCrud.vue
@@ -1,47 +1,42 @@
 <template>
-    <div class="modal" v-bind:class="{'is-active': isActive}">
-        <div class="modal-background"></div>
-            <div class="modal-content">
-                <section>
-                    <h2>Database</h2>
-                    <form v-on:submit.prevent="submit">
-
-                    <div class="field">
-                        <label class="label">Name</label>
-                            <div class="control">
-                                <input class="input" 
-                                       type="text" 
-                                       v-model="form.name"
-                                       placeholder="e.g mydatabase">
-                            </div>
-                    </div>
-                    <div class="field is-grouped is-grouped-right">
-                        <p class="control">
-                            <button type="submit" class="button is-primary">
-                                Submit
-                            </button>
-                        </p>
-                        <p class="control">
-                            <a class="button is-light">
+    <modal>
+        <section class="form-content">
+            <h2 class="title is-2">Creating Database</h2>
+            <form v-on:submit.prevent="submit">
+                <div class="field">
+                    <label class="label">Name</label>
+                        <div class="control">
+                            <input class="input" 
+                                type="text" 
+                                v-model="form.name"
+                                placeholder="e.g mydatabase">
+                        </div>
+                </div>
+                <div class="field is-grouped is-grouped-right">
+                    <p class="control">
+                        <button type="submit" class="button is-primary">
+                            Submit
+                        </button>
+                    </p>
+                    <p class="control">
+                        <button v-on:click="close" type="reset" class="button is-light">
                             Cancel
-                            </a>
-                        </p>
-                    </div>
-                    </form>
-                </section>
-            </div>
-        <button v-on:click="close" class="modal-close is-large" aria-label="close"></button>
-    </div>
+                        </button>
+                    </p>
+                </div>
+                </form>
+        </section>
+    </modal>
 </template>
 
 <script>
+import Modal from '../../components/modal/Modal'
 import axios from 'axios'
 
     export default {
         name: 'db-crud',
         data(){
             return {
-                isActive: true,
                 form: {
                     name: ''
                 }
@@ -49,17 +44,22 @@ import axios from 'axios'
         },
         methods: {
             close: function(){
-                this.isActive = false
             },
             submit: function(){
                 axios.post('http://localhost:4321/db/create', this.form.name)
             }
+        },
+        components: {
+            Modal
         }
     }
 </script>
 
 <style scoped>
-.modal-content{
+.form-content{
     background-color: white;
+    padding: 3vh;
+    border-radius: 3vh;
+    height: 50vh;
 }
 </style>
