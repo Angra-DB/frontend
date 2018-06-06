a42ab66138dbf375df013c3d0495ac741fa91576
Danilo Santos <dandesenvolvimento@gmail.com>
2018-06-06 18:28:29 -0300

    Created Modal component

---
+++ src/components/modal/Modal.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="modal" v-bind:class="{'is-active': isActive}">
+        <div class="modal-background"></div>
+            <div class="modal-content">
+                <slot></slot>
+            </div>
+        <button v-on:click="close" class="modal-close is-large" aria-label="close"></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'modal',
+        data(){
+            return {
+                isActive: true
+            }
+        },
+        methods: {
+            close: function(){
+                this.isActive = false
+            },
+            open: function(){
+                this.isActive = true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
