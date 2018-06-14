ae04e76f950c67545bd375c1da65c45029d36838
Danilo Santos <dandesenvolvimento@gmail.com>
2018-06-14 10:50:31 -0300

    Adding section style to home page

--- src/pages/homepage/HomePage.vue
+++ src/pages/homepage/HomePage.vue
@@ -1,8 +1,10 @@
 <template>
+<section class="section">
     <article class="content has-text-centered">
     <h1 class="title is-1">Welcome to AngraDB</h1>
     <p>A research of funcional database, using Erlang</p>
     </article>
+</section>
 </template>
 
 <script>
